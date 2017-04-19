47256dc00c33af257ddc0540c32709cfc0383d1d
Anthony Scalabrino <anthonyscalabrino@MacBook-Pro.local>
2017-04-19 18:36:13 -0400

    debug a bit

--- TestClient.py
+++ TestClient.py
@@ -13,12 +13,15 @@ global window_size
 global file_start 
 global original_sequence
 global copy_sequence
+global file_name
 original_sequence = []
 copy_sequence = []
 global timer_list 
 timer_list = []
+global data_packet
+global sock
 
-sequence_number = 00000000000000000000000000000000
+s = 00000000000000000000000000000000	
 checksum = 0000000000000000
 data_packet = 0101010101010101
 SERVER_IP = "127.0.0.1"
@@ -35,12 +38,14 @@ window_size = int(window_size)
 MSS = int(MSS)
 
 #ESTABLISHED CONNECTION BASED OFF OF PORT SENT
-c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # c.connect((SERVER_IP, server_port))
 #c.send("HELLO FROM THE CLIENT")
 #hello = c.recv(MSS)
 #print hello 
 # data = 
+
+
 
 def read_file(file):
 	file_start=0
@@ -53,25 +58,27 @@ def read_file(file):
 	if entire_list == "":
 		sock.close()
 	else:
-		rdt_send()
+		rdt_send(file_name, window_size, entire_list, sock, s, timer_list)
+
 
 # Carry bit used in one's combliment
 def checksum_addition(num_1, num_2):
-    c = num_1 + num_2
-    return (c & 0xffff) + (c >> 16)
+    d = num_1 + num_2
+    return (d & 0xffff) + (d >> 16)
 
 
 # Calculate the checksum of the data only. Return True or False
 def calculate_checksum(dta):
     checksum = 0
     for i in range(0, len(dta), 2):
-        message = str(dat)
+        message = str(dta)
         w = ord(message[i]) + (ord(message[i+1]) << 8)
         checksum = checksum_addition(checksum, w)
     return (not checksum) & 0xfff
 
-def rdt_send(file, size, check, dta, sock):
+def rdt_send(file, size, entire_list, sock, sequence_number, timer_list):
 	#  Caluclate sequence number
+	print "ENTERING RDT_SEND"
 	file_start = sequence_number * (size-8)
 
 	#clear the original seq. array 
@@ -84,32 +91,41 @@ def rdt_send(file, size, check, dta, sock):
 		else:
 			to_send = []
 			to_send.append(sequence_number)
-			to_send.append(check)
-			to_send.append(check_sum(dta))
+			#to_send.append(check)
+			
 			if (file_start+(size-8)) > len(entire_list):
-				to_send.append(entire_list[file_start:len(entire_list)])
+				dta = entire_list[file_start:len(entire_list)]
+				to_send.append(calculate_checksum(dta))
+				to_send.append(data_packet)
+				to_send.append(dta)
 				packet = pickle.dumps(to_send)
 				sock.sendto(packet, (SERVER_IP, server_port))
+				# Start Timer in a list 
+				timer_list.append(time.time())
 				file_start=file_start+(size-8)
 				original_sequence.append(sequence_number)
 				sequence_number+=1
-				print "TO SEND ",to_send
+				print "TO SEND (IF PART) ",to_send
 				index+=1
 			else:
-				to_send.append(entire_list[file_start:file_start+(size-8)])
+				dta = entire_list[file_start:file_start+(size-8)]
+				to_send.append(calculate_checksum(dta))
+				to_send.append(data_packet)
+				to_send.append(dta)
 				packet = pickle.dumps(to_send)
 				sock.sendto(packet, (SERVER_IP, server_port))
+				# Start Timer in a list 
+				timer_list.append(time.time())
 				file_start=file_start+(size-8)
 				original_sequence.append(sequence_number)
 				sequence_number+=1
-				print "TO SEND ",to_send
+				print "TO SEND (ELSE PART)",to_send
 				index+=1
-	# Start Timer in a list 
-	timer_list.append(time.time())
-	wait_for_ack(sock)
-
-def wait_for_ack(sock):
 	
+	wait_for_ack(sock, sequence_number, timer_list)
+
+def wait_for_ack(sock, sequence_number, timer_list):
+	print "ENTERING WAIT FOR ACK"
 	ack_count=0
 	
 	for i in original_sequence:
@@ -117,8 +133,9 @@ def wait_for_ack(sock):
 		
 	#check to see if timers have expired: replace with while loop
 	#while (time.time() - timer) >= 10:
-	while (time.time() - timer_list[i]) < 10:
-		recv_packet, addr = s.recvfrom(MSS)
+	min_time = min(timer_list)
+	while (time.time() - min_time) < 10:
+		recv_packet, addr = sock.recvfrom(MSS)
 		recv_packet = pickle.loads(recv_packet)
 
 		ack_sequence = recv_packet[0]
@@ -137,6 +154,7 @@ def wait_for_ack(sock):
 					if ack_sequence == copy_sequence[i]:
 						# Remove this
 						copy_sequence.remove(copy_sequence[i])
+						timer_list.remove(timer_list[i])
 						ack_count+=1
 						#SHOULDNT WE SEND ANOTHER PACKET NOW THAT WE RECEIVED AN ACK... CALL RDT SEND? and st seqnum to min(seqnum)
 
@@ -148,11 +166,11 @@ def wait_for_ack(sock):
 		else:
 			# Handle error - ACK number out of bounds
 			print "ACK number out of bound!"
-
+		min_time = min(timer_list)
 	sequence_number = min(copy_sequence)
 	copy_sequence = []
 	timer_list = []
-	rdt_send(sock)
+	rdt_send(file_name, window_size, entire_list, sock, sequence_number, timer_list)
 
 
 
@@ -173,6 +191,7 @@ def wait_for_ack(sock):
 		# else:
 			#Need to wait until next ACK and window to advance to send next
 			#if window == 1 
-			#c.sendall(file)		
+			#c.sendall(file)
+
 read_file(file_name)
 # rdt_send(file_name, window_size, MSS, sequence_number, checksum, data_packet, c)
